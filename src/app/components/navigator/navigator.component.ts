edaa6543a7614940617f42d68cbf3c820bcca9a3
Tefa600 <67193168+Tefa600@users.noreply.github.com>
2022-08-13 13:35:39 +0200

    This is the initial commit to the draft project, the purpose is to test the push process from IDE

---
+++ src/app/components/navigator/navigator.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+class PhotoService {
+}
+
+@Component({
+  selector: 'app-navigator',
+  templateUrl: './navigator.component.html',
+  styleUrls: ['./navigator.component.scss']
+})
+export class NavigatorComponent implements OnInit {
+
+
+
+
+  responsiveOptions:any[] = [
+    {
+      breakpoint: '1024px',
+      numVisible: 5
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 3
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1
+    }
+  ];
+  images: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
